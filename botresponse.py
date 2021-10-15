eff59bb301f970d1ba66418b3d1eaeff299e1ee6
Vic Kem <resonance@tuta.io>
2021-10-15 05:03:37 -0400

    Added some code.

--- botresponse.py
+++ botresponse.py
@@ -1,290 +1,158 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 
+# logging info into file
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+    filename='logs.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# function to handle the /start command
-def startcommand(update:Update, context: CallbackContext) -> None:
-    reply_buttons = InlineKeyboardMarkup\
-            (
-                [
-                    [
-                        InlineKeyboardButton("Cargo", callback_data='list_of_cargo'),
-                        InlineKeyboardButton("AA Guns", callback_data='list_of_AA_guns')
-                    ],
-                    [
-                        InlineKeyboardButton("ASW", callback_data='ASW'),
-                        InlineKeyboardButton("Auxilary", callback_data='AUX')
-                    ],
-                    [
-                        InlineKeyboardButton("BB guns", callback_data='BB'),
-                        InlineKeyboardButton("DD guns", callback_data='DD')
-                    ],
-                    [
-                        InlineKeyboardButton("Dive bombers", callback_data='DiveB'),
-                        InlineKeyboardButton("Fighters", callback_data='Fighters')
-                    ],
-                    [
-                        InlineKeyboardButton("Heavy Cruiser guns", callback_data='CA_guns'),
-                        InlineKeyboardButton("Large Cruiser guns", callback_data='CL_guns')
-                    ],
-                    [
-                        InlineKeyboardButton("Light Cruiser guns", callback_data='LC_guns'),
-                        InlineKeyboardButton("Seaplanes", callback_data='Seaplanes')
-                    ],
-                    [
-                        InlineKeyboardButton("Sub torpedoes", callback_data='SUB'),
-                        InlineKeyboardButton("Torpedo bombers", callback_data='TorpBomb')
-                    ],
-                    [
-                        InlineKeyboardButton("Torpedoes", callback_data='Torps')
-                    ]
-                ]
-            )
-    first_name = update.message.chat.first_name
-    update.message.reply_text\
-            (
-            f'Welcome to the Azur Lane Equipment bot, {first_name}, what type of equip are you interested in?',
-            reply_markup=reply_buttons
-            )
+# read bot token
+fo = open("bot_data")
+fr = fo.read()
+bot = Bot(token=fr)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-# So, my guess about this chunk of code is either we have buttons, user
-# clicks on particular one and then gets info about coresponding equip
-# or he clicks the equip type from prev menu and then enters the name of
-# the equip into the chat message like - "@alequipment_bot type94"
-# Or we scrap the shitty button idea and communicate with our bot
-# only through inline chat messages
-def button_press(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    if query.data == 'list_of_cargo':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                 [
-                     InlineKeyboardButton("Type94", callback_data='list_of_cargo')
-                 ],
-                 [
-                     InlineKeyboardButton("Aviation materials", callback_data='list_of_cargo')
-                 ],
-                 [
-                     InlineKeyboardButton("Small-Caliber Naval Gun Parts", callback_data='list_of_cargo')
-                 ],
-                 [
-                     InlineKeyboardButton("Torpedo Materials", callback_data='list_of_cargo')
-                 ],
-                 [
-                     InlineKeyboardButton("Go back", callback_data='Go back'),
-                 ],
-            ]
-        ))
+# States
+class Form(StatesGroup):
+    user_message = State()
 
-    #   Get the user to the previous menu
-    if query.data == 'Go back':
-        query.edit_message_text(text='Getting you back')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                [
-                    InlineKeyboardButton("Cargo", callback_data='list_of_cargo'),
-                    InlineKeyboardButton("AA Guns", callback_data='list_of_AA_guns')
-                ],
-                [
-                    InlineKeyboardButton("ASW", callback_data='ASW'),
-                    InlineKeyboardButton("Auxilary", callback_data='AUX')
-                ],
-                [
-                    InlineKeyboardButton("BB guns", callback_data='BB'),
-                    InlineKeyboardButton("DD guns", callback_data='DD')
-                ],
-                [
-                    InlineKeyboardButton("Dive bombers", callback_data='DiveB'),
-                    InlineKeyboardButton("Fighters", callback_data='Fighters')
-                ],
-                [
-                    InlineKeyboardButton("Heavy Cruiser guns", callback_data='CA_guns'),
-                    InlineKeyboardButton("Large Cruiser guns", callback_data='CL_guns')
-                ],
-                [
-                    InlineKeyboardButton("Light Cruiser guns", callback_data='LC_guns'),
-                    InlineKeyboardButton("Seaplanes", callback_data='Seaplanes')
-                ],
-                [
-                    InlineKeyboardButton("Sub torpedoes", callback_data='SUB'),
-                    InlineKeyboardButton("Torpedo bombers", callback_data='TorpBomb')
-                ],
-                [
-                    InlineKeyboardButton("Torpedoes", callback_data='Torps')
-                ]
-            ]
-        ))
+# This handler will be called when user sends /start command
+@dp.message_handler(commands=['start'])
+async def startcommand(message: types.Message):
+    await message.answer(f'Welcome to Azur Lane quipment bot, use this bot to get AL equipment info from AL wiki.\n'
+                        f'Enter name of the equipment and press ">".\n'
+                        )
 
-    elif query.data == 'list_of_AA_guns':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'ASW':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'AUX':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'BB':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'DD':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'DiveB':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'Fighters':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'CA_guns':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'CL_guns':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'LC_guns':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'Seaplanes':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'SUB':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'TorpBomb':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
-    elif query.data == 'Torps':
-        query.edit_message_text(text=f'You chose {query.data}, if you want to get to the previous menu, press "back"')
-        query.edit_message_reply_markup(InlineKeyboardMarkup
-        (
-            [
-                # Create a list of lists for buttons.
-            ]
-        ))
+# This handler will be called when user sends /help command
+@dp.message_handler(commands=['help'])
+async def helpcommand(message: types.Message):
+    await message.answer('Here is a list of all available commands:\n '
+                        '/start - start the bot\n'
+                        '/help - get all available commands\n'
+                        '/update - update the database of equip\n'
+                        )
 
-# function to handle the /help command
-def helpcommand(update:Update, context: CallbackContext) -> None:
-    update.message.reply_text('Here is a list of all available commands:\n '
-                              '/start - start the bot\n'
-                              '/help - get all available commands\n'
-                              '/update - get new equip from the tables\n')
-
-# function that acquires new equipment from the tables
-# so that I don't have to add it manually
-def updatecommand(update:Update, context: CallbackContext) -> None:
-    update.message.reply_text('This feature is not ready YET')
-
-# function to handle errors occured in the dispatcher
-def errormsg(update:Update, context: CallbackContext) -> None:
-    update.message.reply_text('An error occured.')
+# This handler will be called when user sends /update command
+@dp.message_handler(commands=['update'])
+async def updatecommand(message: types.Message):
+    await message.answer(f'This function is not ready yet.\n')
 
 
-# function to handle normal text
-# bot receives the message '@alequipment_bot name of the equip'
-# and then gives equip info to the user
-def text(update:Update, context: CallbackContext) -> None:
-    text_received = update.message.text
-    update.message.reply_text(f'I am only working with commands and not your "{text_received}".')
 
-# main function
-def main():
-    fo = open("bot_data")
-    fr = fo.read()
-    updater = Updater(fr)
-    dispatcher = updater.dispatcher
+#   A bunch of functions to display the coresponding item information
+async def Cargo_func(item):
+    pass
 
-    # add handlers for start and help commands
-    dispatcher.add_handler(CommandHandler("start", startcommand))
-    dispatcher.add_handler(CommandHandler("help", helpcommand))
-    dispatcher.add_handler(CommandHandler("update", updatecommand))
-    dispatcher.add_handler(CallbackQueryHandler(button_press))
+@dp.message_handler()
+async def AA_func(AA_guns):
+    pass
 
-    # add an handler for normal text (not commands)
-    dispatcher.add_handler(MessageHandler(Filters.text, text))
+@dp.message_handler()
+async def ASW_func(ASW):
+    pass
 
-    # add an handler for errors
-    dispatcher.add_error_handler(errormsg)
+@dp.message_handler()
+async def Aux_func(Auxilary):
+    pass
 
-    # start bot
-    updater.start_polling()
+@dp.message_handler()
+async def BB_func(BB_guns):
+    pass
 
-    # run the bot
-    updater.idle()
+@dp.message_handler()
+async def DD_func(DD_guns):
+    pass
+
+@dp.message_handler()
+async def DiveB_func(Dive_Bombers):
+    pass
+
+@dp.message_handler()
+async def Fighters_func(Fighters):
+    pass
+
+@dp.message_handler()
+async def HeavyC_func(Heavy_Cruiser_guns):
+    pass
+
+@dp.message_handler()
+async def LargeC_func(Large_Cruisers_guns):
+    pass
+
+@dp.message_handler()
+async def LightC_func(Light_Cruiser_guns):
+    pass
+
+@dp.message_handler()
+async def Seaplanes_func(Seaplanes):
+    pass
+
+@dp.message_handler()
+async def SubTorps_func(Submarine_Torpedoes):
+    pass
+
+@dp.message_handler()
+async def TorpBomb_func(Torpedo_Bombers):
+    pass
+
+@dp.message_handler()
+async def Torpedoes_func(Torpedoes):
+    pass
+
+#   Table names defined in a dictionary
+table_names = {'Cargo': Cargo_func,
+                'AA Guns' : AA_func,
+                'ASW': ASW_func,
+                'Auxilary': Aux_func,
+                'BB guns': BB_func,
+                'DD guns': DD_func,
+                'Dive bombers' : DiveB_func,
+                'Fighters' : Fighters_func,
+                'Heavy Cruiser guns': HeavyC_func,
+                'Large Cruiser guns': LargeC_func,
+                'Light Cruiser guns' : LightC_func,
+                'Seaplanes' : Seaplanes_func,
+                'Sub torpedoes' : SubTorps_func,
+                'Torpedo bombers': TorpBomb_func,
+                'Torpedoes': Torpedoes_func}
+
+user_input = input("")
+table_names.get(user_input)(user_input)
+
+#   'content_types=types.ContentType.ANY' means that we are dealing with any content type
+#   which is not a text message, by default it's = 'content_types=types.ContentType.TEXT' or something like that
+@dp.message_handler(content_types=types.ContentType.PHOTO)
+async def echo(message: types.Message):
+
+    # Send chat actions
+    await types.ChatActions.upload_photo()
+
+    # Create media group
+    media = types.MediaGroup()
+
+    # Here, the bot will take a picture, sent by the user and store it somewhere
+    media.attach_photo('https://hddesktopwallpapers.in/wp-content/uploads/2015/09/wallpapers-cats-hd.jpg')
+
+    # Send media group
+    await message.answer_media_group(media=media)
+
+@dp.message_handler(content_types=types.ContentType.DOCUMENT)
+async def echo(message: types.Message):
+
+    # Send chat actions
+    await types.ChatActions.upload_document()
+
+    # Create media group
+    media = types.MediaGroup()
+
+    # Here, the bot will take a picture, sent by the user and store it somewhere
+    media.attach_document('https://hddesktopwallpapers.in/wp-content/uploads/2015/09/wallpapers-cats-hd.jpg')
+
+    # Send media group
+    await message.answer_media_group(media=media)
 
 if __name__ == '__main__':
-    main()
-
+    executor.start_polling(dp, skip_updates=True)
